c6cc5a3ac7697cdcb3477dd48ea98f687fb6626f
Emon <emon.mhk69@gmail.com>
2023-05-08 00:00:06 +0600

    intial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware:
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("express coffe house is running");
+})
+
+app.listen(port , (req, res)=>{
+    console.log(`express coffe house is running on port: ${port}`);
+});
